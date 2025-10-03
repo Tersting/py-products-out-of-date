4ff8a98638be27aca327626ed0f2118094746510
Nazar <your@mail.com>
2025-10-03 12:49:19 +0300

    I fuck tests_2

--- app/test_main.py
+++ app/test_main.py
@@ -9,44 +9,57 @@ from app.main import outdated_products
     [
         (
             [
-                {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10)},
-                {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5)},
-                {"name": "duck", "expiration_date": datetime.date(2022, 2, 1)},
+                {"name": "salmon", "expiration_date":
+                    datetime.date(2022, 2, 10)},
+                {"name": "chicken", "expiration_date":
+                    datetime.date(2022, 2, 5)},
+                {"name": "duck", "expiration_date":
+                    datetime.date(2022, 2, 1)},
             ],
             datetime.date(2022, 2, 10),
             ["chicken", "duck"],
         ),
         (
             [
-                {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10)},
-                {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5)},
-                {"name": "duck", "expiration_date": datetime.date(2022, 2, 1)},
+                {"name": "salmon", "expiration_date":
+                    datetime.date(2022, 2, 10)},
+                {"name": "chicken", "expiration_date":
+                    datetime.date(2022, 2, 5)},
+                {"name": "duck", "expiration_date":
+                    datetime.date(2022, 2, 1)},
             ],
             datetime.date(2022, 2, 6),
             ["chicken", "duck"],
         ),
         (
             [
-                {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10)},
-                {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5)},
-                {"name": "duck", "expiration_date": datetime.date(2022, 2, 1)},
+                {"name": "salmon", "expiration_date":
+                    datetime.date(2022, 2, 10)},
+                {"name": "chicken", "expiration_date":
+                    datetime.date(2022, 2, 5)},
+                {"name": "duck", "expiration_date":
+                    datetime.date(2022, 2, 1)},
             ],
             datetime.date(2022, 2, 2),
             ["duck"],
         ),
         (
             [
-                {"name": "salmon", "expiration_date": datetime.date(2022, 2, 1)},
-                {"name": "chicken", "expiration_date": datetime.date(2022, 1, 31)},
+                {"name": "salmon", "expiration_date":
+                    datetime.date(2022, 2, 1)},
+                {"name": "chicken", "expiration_date":
+                    datetime.date(2022, 1, 31)},
             ],
             datetime.date(2022, 2, 2),
             ["salmon", "chicken"],
         ),
     ],
 )
-def test_outdated_products(products: list[dict], today: datetime.date, expected: list[str]) -> None:
+def test_outdated_products(products: list[dict], today: datetime.date,
+                           expected: list[str]) -> None:
     with mock.patch("app.main.datetime.date") as mock_date:
         mock_date.today.return_value = today
-        mock_date.side_effect = lambda *args, **kwargs: datetime.date(*args, **kwargs)
+        mock_date.side_effect = lambda *args, **kwargs: datetime.date(
+            *args, **kwargs)
 
         assert outdated_products(products) == expected
